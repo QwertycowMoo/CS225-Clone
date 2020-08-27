fafe0ffdaa53d6b4a0584bf2abbc3292c0311a3a
Kevin Zhou <kjzhou2@illinois.edu>
2020-08-27 16:33:41 -0500

    Constructor now with restrictions

--- lab_intro/cs225/HSLAPixel.cpp
+++ lab_intro/cs225/HSLAPixel.cpp
@@ -19,16 +19,45 @@ namespace cs225 {
         a = 1.0;
     }
     HSLAPixel::HSLAPixel(double hue, double saturation, double luminance) {
-        h = hue;
-        s = saturation;
-        l = luminance;
+        if (hue < 360 && hue >= 0) {
+            h = hue;
+        } else {
+            h = 0.0;
+        }
+        if (saturation <= 1 && saturation >= 0) {
+            s = saturation;
+        } else {
+            s = 0.0;
+        }
+        if (luminance <= 1 && luminance >= 0) {
+            l = luminance;
+        } else {
+            l = 1.0;
+        }
         a = 1.0;
     }
     HSLAPixel::HSLAPixel(double hue, double saturation, double luminance, double alpha){
-        h = hue;
-        s = saturation;
-        l = luminance;
-        a = alpha;
+        if (hue < 360 && hue >= 0) {
+            h = hue;
+        } else {
+            h = 0.0;
+        }
+        if (saturation <= 1 && saturation >= 0) {
+            s = saturation;
+        } else {
+            s = 0.0;
+        }
+        if (luminance <= 1 && luminance >= 0) {
+            l = luminance;
+        } else {
+            l = 1.0;
+        }
+        if (alpha <= 1 && luminance >= 0) {
+            a = alpha;
+        } else {
+            a = 1.0;
+        }
+        
     }
         
 }
