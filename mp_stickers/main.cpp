4a1de969d53d85df8d4b114c7facad32f4485f8b
Kevin Zhou <kjzhou2@illinois.edu>
2020-09-21 16:27:59 -0500

    full sticker sheet with myImage

--- mp_stickers/main.cpp
+++ mp_stickers/main.cpp
@@ -11,22 +11,24 @@ int main() {
   // Reminder:
   //   Before exiting main, save your creation to disk as myImage.png
   //
-  Image alma; alma.readFromFile("tests/alma.png");
-  Image i;    i.readFromFile("tests/i.png");
-  Image expected; expected.readFromFile("tests/expected.png");
-  Image expected2; expected2.readFromFile("tests/expected-2.png");
+  Image pusheen; pusheen.readFromFile("pusheen.png");
+  Image music;    music.readFromFile("music.png");
+  Image dnd; dnd.readFromFile("dndD20.png");
+  Image background; background.readFromFile("846697.png");
 
-  StickerSheet s1(alma, 5);
-  s1.addSticker(i, 20, 200);
-  s1.addSticker(i, 40, 200);
+  StickerSheet s1(background, 7);
+  s1.addSticker(pusheen, 20, 200);
+  s1.addSticker(pusheen, 70, 500);
+  s1.addSticker(music, 200, 200);
+  s1.addSticker(music, 1000, 600);
+  s1.addSticker(pusheen, 1250, 400);
+  s1.addSticker(dnd, 600, 100);
+  s1.addSticker(dnd, 1350, 0);
 
-  StickerSheet s2(alma, 5);
-  s2 = s1;
-  s2.removeSticker(1);
+
 
   Image render = s1.render();
-  Image render2 = s2.render();
-  render.writeToFile("myImage-1.png");
-  render2.writeToFile("myImage-2.png");
+  render.writeToFile("myImage.png");
+
   return 0;
 }
